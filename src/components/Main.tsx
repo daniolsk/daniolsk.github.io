07cf67478c6ba418fc33e84dd993e3b7ace5f416
daniolsk <danio.skowron@gmail.com>
2023-06-08 00:26:20 +0200

    move styles to tailwind

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -30,82 +30,46 @@ const Main = () => {
       ref={sectionRef}
       className={`${style.section} relative flex min-h-screen w-full items-start justify-start overflow-hidden pl-[10vw] md:items-center`}
     >
-      <motion.div
-        initial={{ backgroundSize: "3vmin 3vmin" }}
-        animate={{ backgroundSize: "4vmin 4vmin" }}
-        className={`${style.bgPattern}`}
-      ></motion.div>
+      <div
+        className={`${style.bgPattern} bg-dots absolute left-0 top-0 -z-20 h-[100vh] w-full bg-[length:4vmin_4vmin] bg-[0%_-35%] opacity-70 transition-all duration-700 ease-out`}
+      ></div>
       <div className="flex flex-col items-start">
         <div className="mb-8 mt-8 md:mb-10 md:mt-0 xl:mb-16" ref={mainTitleRef}>
-          <motion.div
-            initial={{ x: -30, opacity: 0 }}
-            animate={{ x: 0, opacity: 1, transition: { delay: 0.2 } }}
-            className="whitespace-nowrap text-4xl font-bold tracking-tighter sm:text-4xl md:text-5xl 2xl:text-6xl"
-          >
+          <div className="whitespace-nowrap text-4xl font-bold tracking-tighter sm:text-4xl md:text-5xl 2xl:text-6xl">
             Daniel Skowron
-          </motion.div>
-          <motion.div
-            initial={{ x: -30, opacity: 0 }}
-            animate={{ x: 0, opacity: 1, transition: { delay: 0.3 } }}
-            className="font-normal sm:text-lg md:text-xl 2xl:text-2xl"
-          >
+          </div>
+          <div className="font-normal sm:text-lg md:text-xl 2xl:text-2xl">
             Web developer and designer.
-          </motion.div>
+          </div>
         </div>
         <div
           ref={menuRef}
-          className={`${style.menu} flex flex-col items-start gap-4 md:gap-6 xl:gap-8`}
+          className={`${style.menu} group flex flex-col items-start gap-4 md:gap-6 xl:gap-8`}
         >
-          <motion.button
-            initial={{ x: -30, opacity: 0 }}
-            animate={{
-              x: 0,
-              opacity: 1,
-              transition: { delay: 0.5, ease: "easeOut" },
-            }}
+          <button
             onClick={() => {
               setActiveTab(0);
             }}
-            className={`${style.menuElement} ${
-              activeTab == 0 ? "!translate-x-[30px]" : ""
-            } cursor-pointer p-2 text-lg font-medium md:text-3xl xl:text-4xl`}
+            className={`${style.menuElement} relative cursor-pointer p-2 text-lg font-medium transition-all duration-300 ease-out after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-[0%] after:bg-white after:transition-all after:duration-300 after:ease-out after:content-[''] hover:translate-x-8 hover:!opacity-100 hover:after:w-[100%] group-hover:opacity-30 md:text-3xl xl:text-4xl`}
           >
             About Me
-          </motion.button>
-          <motion.button
-            initial={{ x: -30, opacity: 0 }}
-            animate={{
-              x: 0,
-              opacity: 1,
-              transition: { delay: 0.6, ease: "easeOut" },
-            }}
-            active={activeTab == 1 ? true : false}
+          </button>
+          <button
             onClick={() => {
               setActiveTab(1);
             }}
-            className={`${style.menuElement} ${
-              activeTab == 1 ? "!translate-x-[30px]" : ""
-            } cursor-pointer p-2 text-lg font-medium md:text-3xl xl:text-4xl`}
+            className={`${style.menuElement} relative cursor-pointer p-2 text-lg font-medium transition-all duration-300 ease-out after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-[0%] after:bg-white after:transition-all after:duration-300 after:ease-out after:content-[''] hover:translate-x-8 hover:!opacity-100 hover:after:w-[100%] group-hover:opacity-30 md:text-3xl xl:text-4xl`}
           >
             Projects
-          </motion.button>
-          <motion.button
-            initial={{ x: -30, opacity: 0 }}
-            animate={{
-              x: 0,
-              opacity: 1,
-              transition: { delay: 0.7, ease: "easeOut" },
-            }}
+          </button>
+          <button
             onClick={() => {
               setActiveTab(2);
             }}
-            active={activeTab == 2 ? true : false}
-            className={`${style.menuElement} ${
-              activeTab == 2 ? "!translate-x-[30px]" : ""
-            } cursor-pointer p-2 text-lg font-medium md:text-3xl xl:text-4xl`}
+            className={`${style.menuElement} relative cursor-pointer p-2 text-lg font-medium transition-all duration-300 ease-out after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-[0%] after:bg-white after:transition-all after:duration-300 after:ease-out after:content-[''] hover:translate-x-8 hover:!opacity-100 hover:after:w-[100%] group-hover:opacity-30 md:text-3xl xl:text-4xl`}
           >
             Contact
-          </motion.button>
+          </button>
         </div>
       </div>
     </section>

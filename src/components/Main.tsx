73482603c1a42b408c12816829058dba09780929
daniolsk <danio.skowron@gmail.com>
2023-06-08 01:42:29 +0200

    add animations

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -45,14 +45,14 @@ const Main = () => {
       className={`${style.section} relative flex min-h-screen w-full flex-1 items-start justify-center overflow-hidden md:items-center md:justify-start md:pl-[10vw]`}
     >
       <div
-        className={`${style.bgPattern} bg-dots absolute left-0 top-0 -z-20 h-[100vh] w-full bg-[length:6vmin_6vmin] bg-[0%_-35%] opacity-80 transition-all duration-700 ease-out md:bg-[length:4vmin_4vmin]`}
+        className={`${style.bgPattern} animate-slideleft-bg bg-dots absolute left-0 top-0 -z-20 h-[100vh] w-full bg-[length:6vmin_6vmin] bg-[0%_-35%] opacity-80 transition-all duration-700 ease-out md:bg-[length:4vmin_4vmin]`}
       ></div>
       <div className="flex flex-col items-center  md:items-start">
         <div className="mb-8 mt-8 md:mb-10 md:mt-0 xl:mb-16" ref={mainTitleRef}>
-          <div className="whitespace-nowrap text-4xl font-bold tracking-tighter sm:text-4xl md:text-5xl 2xl:text-6xl">
+          <div className="animation-delay-200 animate-fadein whitespace-nowrap text-4xl font-bold tracking-tighter sm:text-4xl md:text-5xl 2xl:text-6xl">
             Daniel Skowron
           </div>
-          <div className="font-normal sm:text-lg md:text-xl 2xl:text-2xl">
+          <div className="animation-delay-300 animate-fadein font-normal sm:text-lg md:text-xl 2xl:text-2xl">
             Web developer and designer.
           </div>
         </div>
@@ -66,7 +66,7 @@ const Main = () => {
             }}
             className={`${style.menuElement} ${
               activeTab == 0 ? "after:w-[100%]" : ""
-            } relative cursor-pointer p-2 text-lg font-medium transition-all duration-300 ease-out after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-[0%] after:bg-white after:transition-all after:duration-300 after:ease-out after:content-[''] hover:!opacity-100 hover:after:w-[100%] group-hover:opacity-30 md:text-xl md:hover:translate-x-4 md:hover:translate-y-0 xl:text-3xl`}
+            } animation-delay-500 animate-slideleft relative cursor-pointer p-2 text-lg font-medium transition-all duration-300 ease-out after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-[0%] after:bg-white after:transition-all after:duration-300 after:ease-out after:content-[''] hover:!opacity-100 hover:after:w-[100%] group-hover:opacity-30 md:text-xl md:hover:translate-x-4 md:hover:translate-y-0 xl:text-3xl`}
           >
             About Me
           </button>
@@ -76,7 +76,7 @@ const Main = () => {
             }}
             className={`${style.menuElement} ${
               activeTab == 1 ? "after:w-[100%]" : ""
-            } relative cursor-pointer p-2 text-lg font-medium transition-all duration-300 ease-out after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-[0%] after:bg-white after:transition-all after:duration-300 after:ease-out after:content-[''] hover:!opacity-100 hover:after:w-[100%] group-hover:opacity-30 md:text-xl md:hover:translate-x-4 md:hover:translate-y-0 xl:text-3xl`}
+            } animation-delay-600 animate-slideleft relative cursor-pointer p-2 text-lg font-medium transition-all duration-300 ease-out after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-[0%] after:bg-white after:transition-all after:duration-300 after:ease-out after:content-[''] hover:!opacity-100 hover:after:w-[100%] group-hover:opacity-30 md:text-xl md:hover:translate-x-4 md:hover:translate-y-0 xl:text-3xl`}
           >
             Projects
           </button>
@@ -86,7 +86,7 @@ const Main = () => {
             }}
             className={`${style.menuElement} ${
               activeTab == 2 ? "after:w-[100%]" : ""
-            } relative cursor-pointer p-2 text-lg font-medium transition-all duration-300 ease-out after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-[0%] after:bg-white after:transition-all after:duration-300 after:ease-out after:content-[''] hover:!opacity-100 hover:after:w-[100%] group-hover:opacity-30 md:text-xl md:hover:translate-x-4 md:hover:translate-y-0 xl:text-3xl`}
+            } animation-delay-700 animate-slideleft relative cursor-pointer p-2 text-lg font-medium transition-all duration-300 ease-out after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-[0%] after:bg-white after:transition-all after:duration-300 after:ease-out after:content-[''] hover:!opacity-100 hover:after:w-[100%] group-hover:opacity-30 md:text-xl md:hover:translate-x-4 md:hover:translate-y-0 xl:text-3xl`}
           >
             Contact
           </button>

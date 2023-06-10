45f093d2ffa33fe3b6c9d63e8da243e3b1db7ca8
daniolsk <danio.skowron@gmail.com>
2023-06-10 19:57:56 +0200

    static site

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		domains: ['muscler.vercel.app', 'guessthesong.vercel.app'],
-	},
+  output: "export",
+  images: {
+    domains: ["muscler.vercel.app", "guessthesong.vercel.app"],
+  },
 };
 
 module.exports = nextConfig;
